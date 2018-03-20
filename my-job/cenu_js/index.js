7e7e92aa4e7fd0d4b16ce9b766421c878674f2e7
yiuuucen <1784373887@qq.com>
2018-03-20 22:51:55 +0800

    work

---
+++ my-job/cenu_js/index.js
@@ -0,0 +1,8 @@
+//取得随机颜色
+var getRandomColor = function(){
+    return  '#' +
+        (function(color){
+            return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])
+            && (color.length == 6) ?  color : arguments.callee(color);
+        })('');
+}
